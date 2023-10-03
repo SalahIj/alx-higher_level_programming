5c210126c78b7a2e01e6bccd750be856249e0655
SalahIj <ijbasalaheddine@gmail.com>
2023-10-03 20:24:06 +0000

    Task_8

---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def uppercase(str):
+    for i in str:
+        print("{:c}"
+              .format(ord(i) - 32) if (ord(i) >= 97 and ord(i) <= 122)
+              else "{:c}".format(ord(i)), end="")
+    else:
+        print(end="\n")
