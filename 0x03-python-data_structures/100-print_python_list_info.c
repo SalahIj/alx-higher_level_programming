b61a4d2e4638c15a3273643c2e03d7896281af18
SalahIj <ijbasalaheddine@gmail.com>
2023-10-09 14:25:03 +0000

    Task_13

---
+++ 0x03-python-data_structures/100-print_python_list_info.c
@@ -0,0 +1,25 @@
+#include <Python.h>
+
+/**
+ * print_python_list_info -  the function name.
+ * @p: the input of the function
+ */
+
+void print_python_list_info(PyObject *p)
+{
+	int s, allocation,  = 0;
+	Pyobject *ject;
+
+	allocation = ((PylistObject *)p)->allocated;
+	s = Py_SIZE(p);
+	printf("[*] Size of the Python List = %d\n", s);
+	printf("[*] Allocated = %d\n", allocation);
+
+	while (j < s)
+	{
+		printf("Element %d: ", j);
+		ject = PyList_GetItem(p, j);
+		printf("%s\n", (*Py_TYPE(ject)).tp_name);
+		j++;
+	}
+}
