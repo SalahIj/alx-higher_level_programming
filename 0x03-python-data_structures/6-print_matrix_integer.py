7e17f010920d74e863ac95f3ad47b73d21942698
SalahIj <ijbasalaheddine@gmail.com>
2023-10-09 12:08:47 +0000

    Task_6

---
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def print_matrix_integer(matrix=[[]]):
+    if not matrix:
+        return (None)
+    for i in matrix:
+        if (len(i) == 0):
+            print()
+        for index, j in enumerate(i):
+            print(j, end="\n" if index == len(i) - 1 else " ")
