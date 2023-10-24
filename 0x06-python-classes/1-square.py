b96304ab0a76bb5ed6ae493b8e1bf7d0e929ed61
SalahIj <ijbasalaheddine@gmail.com>
2023-10-24 20:19:30 +0000

    Task_1

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -5,10 +5,10 @@
 class Square:
     """ Square class """
 
-    def __init__(self, size):
+    def __init__(self, Square__size):
         """Constractor.
 
         Args:
             size: the first input
         """
-        self._size = size
+        self._Square__size = Square__size
