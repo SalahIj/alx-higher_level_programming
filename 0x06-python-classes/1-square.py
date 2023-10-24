ba2d87612b5db68c524aab8cb56cef768e90b146
SalahIj <ijbasalaheddine@gmail.com>
2023-10-24 20:15:38 +0000

    Task_1

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+""" Square module """
+
+
+class Square:
+    """Constractor.
+
+    Args:
+    size: the first input
+    """
+    def __init__(self, size):
+        self._size = size
