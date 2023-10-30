09eb0320bdb275b791a22fb944f9aec8eb92deae
SalahIj <ijbasalaheddine@gmail.com>
2023-10-30 20:57:52 +0000

    Task_9

--- 0x08-python-more_classes/9-rectangle.py
+++ 0x08-python-more_classes/9-rectangle.py
@@ -15,7 +15,7 @@ class Rectangle:
     @property
     def width(self):
         """ Definition of getter function for width """
-        return (self._width)
+        return (self.__width)
 
     @width.setter
     def width(self, value):
@@ -24,12 +24,12 @@ class Rectangle:
             raise TypeError("width must be an integer")
         if (value < 0):
             raise ValueError("width must be >= 0")
-        self._width = value
+        self.__width = value
 
     @property
     def height(self):
         """ property function for height """
-        return (self._height)
+        return (self.__height)
 
     @height.setter
     def height(self, value):
@@ -38,18 +38,18 @@ class Rectangle:
             raise TypeError("height must be an integer")
         if (value < 0):
             raise ValueError("height must be >= 0")
-        self._height = value
+        self.__height = value
 
     def area(self):
         """ Definition of area function """
-        return (self._height * self._width)
+        return (self.__height * self.__width)
 
     def perimeter(self):
         """ Definition of perimeter function """
-        if (self._height == 0 or self._width == 0):
+        if (self.__height == 0 or self.__width == 0):
             return (0)
         else:
-            return (2 * (self._height + self._width))
+            return (2 * (self.__height + self.__width))
 
     @classmethod
     def square(cls, size=0):
@@ -59,17 +59,17 @@ class Rectangle:
     def __str__(self):
         """ Definition of str function """
         stri = ""
-        if (self._width == 0 or self._height == 0):
+        if (self.__width == 0 or self.__height == 0):
             return (stri)
-        for i in range(self._height):
-            stri += "".join(str(self.print_symbol) * self._width)
-            if (i != self._height - 1):
+        for i in range(self.__height):
+            stri += "".join(str(self.print_symbol) * self.__width)
+            if (i != self.__height - 1):
                 stri += "\n"
         return (stri)
 
     def __repr__(self):
         """ Representation function """
-        return ("Rectangle({:d}, {:d})".format(self._width, self._height))
+        return ("Rectangle({:d}, {:d})".format(self.__width, self.__height))
 
     def __del__(self):
         """ Deleting function for deleting an instance """
