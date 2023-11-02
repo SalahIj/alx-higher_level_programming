c83fc9c180a89f36144ddccc8021905de22848e2
SalahIj <ijbasalaheddine@gmail.com>
2023-11-02 16:16:09 +0000

    Task_0

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -1,11 +1,21 @@
 #!/usr/bin/pytohn3
+"""
+Simple doctest
+test the deco
+with 5 lines
+"""
+
+
 def add_integer(a, b=98):
+    """
+    add function
+    """
     if (not isinstance(a, int) and not isinstance(a, float)):
         raise TypeError("a must be an integer")
     if (not isinstance(b, float) and not isinstance(b, int)):
         raise TypeError("b must be an integer")
-    if (type(a) == float):
+    if (type(a) is float):
         a = int(a)
-    if (type(b) == float):
+    if (type(b) is float):
         b = int(b)
     return (a + b)
