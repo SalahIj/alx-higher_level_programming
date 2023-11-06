f01e9caf22937645d0f4d7caba8ff969ab4daf38
SalahIj <ijbasalaheddine@gmail.com>
2023-11-06 12:09:18 +0000

    Task_3

---
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+""" Module imported """
+
+
+def is_kind_of_class(obj, a_class):
+    """ The function definition """
+    if (isinstance(obj, a_class)):
+        return (True)
+    else:
+        return (False)
