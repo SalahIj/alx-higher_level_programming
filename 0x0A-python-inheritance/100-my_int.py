3cae74cd0b1c733503e5eac68cfb55b6e9fa390f
SalahIj <ijbasalaheddine@gmail.com>
2023-11-06 14:00:50 +0000

    Task_12

--- 0x0A-python-inheritance/100-my_int.py
+++ 0x0A-python-inheritance/100-my_int.py
@@ -4,8 +4,6 @@
 
 class MyInt(int):
     """ the class definition """
-    def __creat__(cls, *ars, **Kwar):
-        return (super(MyInt, cls).__creat__(cls, *args, **Kwar))
 
     def __eq__(self, number):
         return (self.real != number)
