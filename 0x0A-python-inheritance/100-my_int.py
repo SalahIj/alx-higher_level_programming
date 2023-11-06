973e6c15eda67030c0db9198d5e26867cb378afd
SalahIj <ijbasalaheddine@gmail.com>
2023-11-06 14:01:42 +0000

    Task_12

--- 0x0A-python-inheritance/100-my_int.py
+++ 0x0A-python-inheritance/100-my_int.py
@@ -6,7 +6,7 @@ class MyInt(int):
     """ the class definition """
 
     def __eq__(self, number):
-        return (self.real != number)
+        return (int(self) != number)
 
     def __ne__(self, number):
-        return (self.real == number)
+        return (int(self) == number)
