2340afd87289df042a1f2a37dcc2ade441f216c7
SalahIj <ijbasalaheddine@gmail.com>
2023-11-06 12:45:39 +0000

    Task_7

--- 0x0A-python-inheritance/8-rectangle.py
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -10,3 +10,5 @@ class BaseGeometry:
             raise TypeError("{} must be an integer".format(name))
         if (value <= 0):
             raise ValueError("{} must be greater than 0".format(name))
+
+
