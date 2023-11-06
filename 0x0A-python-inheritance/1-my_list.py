44c5589a24dd031f02ceb6a241bd92b2d4120025
SalahIj <ijbasalaheddine@gmail.com>
2023-11-06 11:38:52 +0000

    Task_1

---
+++ 0x0A-python-inheritance/1-my_list.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+""" Module imported"""
+
+
+class MyList(list):
+    """ The class definition"""
+    pass
+
+    def print_sorted(self):
+        """Method of the class"""
+        print(sorted(self))
