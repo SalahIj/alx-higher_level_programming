e568b31ef03cfe424a097313e6c4f62ee730343c
SalahIj <ijbasalaheddine@gmail.com>
2023-11-07 14:45:04 +0000

    Task_3

---
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+""" The module imported """
+import json
+
+
+def to_json_string(my_obj):
+    """ The function definition """
+    string = json.dumps(my_obj)
+    return (string)
