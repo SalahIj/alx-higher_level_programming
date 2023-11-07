ea289aad52868b5041429414be6ad9282a16cf03
SalahIj <ijbasalaheddine@gmail.com>
2023-11-07 16:07:55 +0000

    Task_8

---
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+""" The module imported """
+
+
+def class_to_json(obj):
+    """ The function description:
+    Args:
+        obj: the input of the function
+    Return:
+        the result
+    """
+    return (obj.__dict__)
