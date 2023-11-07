19f1135879b8ed704f99b415f316b1652d8e3e52
SalahIj <ijbasalaheddine@gmail.com>
2023-11-07 15:07:49 +0000

    Task_5

--- 0x0B-python-input_output/5-save_to_json_file.py
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -5,6 +5,5 @@ import json
 
 def save_to_json_file(my_obj, filename):
     """ The function definition """
-    text = json.dumps(my_obj)
-    with open(filename, 'w', encoding="utf-8") as f:
-        f.write(text)
+    with open(filename, 'w', encoding="utf-8") as File:
+        json.dump(my_obj, File)
