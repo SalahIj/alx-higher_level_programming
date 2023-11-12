7fa87cbea1e0fc1a29cd9ddaeb9e0c1d8bb0febe
SalahIj <ijbasalaheddine@gmail.com>
2023-11-12 12:57:19 +0000

    Task_16

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -19,9 +19,9 @@ class Base:
 
     @staticmethod
     def to_json_string(list_dictionaries):
-        """ JSON representation:
+        """ JSON representation method:
         Args:
-            list_dictionaries
+            list_dictionaries: the input of the method
         Return:
             the result
         """
@@ -31,3 +31,21 @@ class Base:
             return (string)
         else:
             return (json.dumps(list_dictionaries))
+
+    @classmethod
+    def save_to_file(cls, list_objs):
+        """ saving to file method:
+        Args:
+            cls: class
+            list_objs: the second input
+        """
+        list_dictionary = []
+        filename = "{}.json".format(cls.__name__)
+
+        if (not list_objs):
+            pass
+        else:
+            list_dictionary = [k.to_dictionary() for k in list_objs]
+        List = cls.to_json_string(list_dictionary)
+        with open(filename, 'w', encoding='utf-8') as File:
+            File.write(List)
