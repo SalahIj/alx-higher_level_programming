2fd00a629250f109bb9e1d825bef0e05b259c3a9
SalahIj <ijbasalaheddine@gmail.com>
2023-11-12 12:30:36 +0000

    Task_15

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,3 +1,7 @@
+import json
+""" imported module """
+
+
 class Base:
     """ The class definition """
     __nb_objects = 0
@@ -12,3 +16,18 @@ class Base:
         else:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
+
+    @staticmethod
+    def to_json_string(list_dictionaries):
+        """ JSON representation:
+        Args:
+            list_dictionaries
+        Return:
+            the result
+        """
+        lenght = len(list_dictionaries)
+        string = "[]"
+        if (list_dictionaries is None or lenght == 0):
+            return (string)
+        else:
+            return (json.dumps(list_dictionaries))
