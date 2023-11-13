53ec6ebf4060d6f7c9b64a91d2fc614e4bd7e76a
SalahIj <ijbasalaheddine@gmail.com>
2023-11-13 20:29:04 +0000

    Task_4

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -70,3 +70,68 @@ class Rectangle(Base):
         if (value < 0):
             raise ValueError("y must be >= 0")
         self.__y = value
+
+    def area(self):
+        """ Area method:
+        Return:
+            the result
+        """
+        return (self.__width * self.__height)
+
+    def display(self):
+        """ Display rectangle method """
+        str_1 = self.y * '\n'
+        str_2 = (" " * self.x)
+        str_3 = ("#" * self.width + "\n")
+        str_4 = (str_2 + str_3) * self.height
+        print(str_1 + str_4, end='')
+
+    def __str__(self):
+        """ overriding method """
+        string = ''
+        string += "[{}] ".format(__class__.__name__)
+        string += "({}) ".format(self.id)
+        string += "{}/{} -".format(self.x, self.y)
+        string += " {}/{}".format(self.__width, self.__height)
+        return (string)
+
+    def display(self):
+        """ Display rectangle method """
+        str_1 = self.y * '\n'
+        str_2 = (" " * self.x)
+        str_3 = ("#" * self.width + "\n")
+        str_4 = (str_2 + str_3) * self.height
+        print(str_1 + str_4, end='')
+
+    def __str__(self):
+        """ overriding method """
+        string = ''
+        string += "[{}] ".format(__class__.__name__)
+        string += "({}) ".format(self.id)
+        string += "{}/{} -".format(self.x, self.y)
+        string += " {}/{}".format(self.__width, self.__height)
+        return (string)
+
+    def update(self, *args, **kwargs):
+        """ Update method:
+        Args:
+            args: the first input
+            kwargs: the second input
+        """
+        list_args = ['id', 'width', 'height', 'x', 'y']
+        lenght = len(args)
+        if (args is not None and lenght != 0):
+            for r in range(lenght):
+                setattr(self, list_args[r], args[r])
+        else:
+            for cle, valeur in kwargs.items():
+                setattr(self, cle, valeur)
+
+    def to_dictionary(self):
+        """ Dict method:
+        Return:
+            the result
+        """
+        Dictio = {'id': self.id, 'width': self.__width,
+                  'height': self.__height, 'x': self.__x, 'y': self.__y}
+        return (Dictio)
