0d488c5fe5f62781234bc4acdf217e99b38eee05
SalahIj <ijbasalaheddine@gmail.com>
2023-11-13 19:43:51 +0000

    Task_0

---
+++ 0x0C-python-almost_a_circl/models/base.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+import json
+import os
+import csv
+""" imported module """
+
+
+class Base:
+    """ The class definition """
+    __nb_objects = 0
+
+    def __init__(self, id=None):
+        """ The constructor method:
+        Args:
+            id: the input of the function
+        """
+        if (id is not None):
+            self.id = id
+        else:
+            Base.__nb_objects += 1
+            self.id = Base.__nb_objects
