f2a8ca3aa477e5f499201cee2975c135699f14c4
SalahIj <ijbasalaheddine@gmail.com>
2023-11-13 09:50:46 +0000

    Task_0

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -55,7 +55,7 @@ class Rectangle(Base):
     def x(self, value):
         """ setter """
         if (type(value) is not int):
-            raise TypeError("y must be an integer")
+            raise TypeError("x must be an integer")
         if (value < 0):
             raise ValueError("x must be >= 0")
         self.__x = value
