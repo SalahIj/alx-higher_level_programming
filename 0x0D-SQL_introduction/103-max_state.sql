63c30ab05b316e8a041d8a0fcca7a22defb5b895
Ijbar Salah Eddine <ijbarbusiness@gmail.com>
2023-12-12 20:32:57 +0000

    Task 17

--- 0x0D-SQL_introduction/103-max_state.sql
+++ 0x0D-SQL_introduction/103-max_state.sql
@@ -1,11 +1,2 @@
--- Use the target database
-USE hbtn_0c_0;
-
--- Convert the database to UTF8
-ALTER DATABASE hbtn_0_0 CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
-
--- Convert the table to UTF8
-ALTER TABLE first_table CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
-
--- Convert the 'name' field to UTF8
-ALTER TABLE first_table MODIFY COLUMN name VARCHAR(256) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+-- Displaying the temperature of every state.
+SELECT `state`, MAX(`value`) AS `max_temp` FROM `temperatures` GROUP BY `state` ORDER BY `state`;
