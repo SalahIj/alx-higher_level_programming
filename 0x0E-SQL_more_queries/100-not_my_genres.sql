c64aaa6d302b5c2e2eb02857c67d25266ed525ae
Ijbar Salah Eddine <ijbarbusiness@gmail.com>
2023-12-13 18:27:37 +0000

    Task_17

--- 0x0E-SQL_more_queries/100-not_my_genres.sql
+++ 0x0E-SQL_more_queries/100-not_my_genres.sql
@@ -1,8 +1,9 @@
 -- Listing all genres.
-SELECT name FROM tv_genres AS
-WHERE tv_genres.id NOT IN (
-	      SELECT tv_genres.id FROM tv_genres
-	      JOIN tv_show_genres AS tg ON tv_genres.id=tg.genre_id
-	      JOIN tv_shows ON tv_shows.id=tg.show_id
-	      WHERE tv_shows.title = "DEXTER" )
-ORDER BY tv_genres.name;
+SELECT g.name FROM tv_genres AS g
+WHERE g.name NOT IN (
+	      SELECT g.name FROM tv_genres AS g
+	      INNER JOIN tv_show_genres AS m ON g.id = m.genre_id
+	      INNER JOIN tv_shows AS s ON m.show_id = s.id
+	      WHERE s.title = 'Dexter'
+)
+ORDER BY g.name ASC;
