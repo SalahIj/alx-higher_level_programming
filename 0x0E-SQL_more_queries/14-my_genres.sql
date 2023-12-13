4f0e2f36dd81be0f960bb854848b411e10da6272
Ijbar Salah Eddine <ijbarbusiness@gmail.com>
2023-12-13 18:07:39 +0000

    Task_14

--- 0x0E-SQL_more_queries/14-my_genres.sql
+++ 0x0E-SQL_more_queries/14-my_genres.sql
@@ -1,6 +1,5 @@
--- Using 'hbtn_0d_tvshows' for listing...
-SELECT tv_genres.name
-FROM tv_genres AS tg
-INNER JOIN tv_show_genres m ON tg.id = m.genre_id
-INNER JOIN tv_shows s ON m.show_id = s.id
-WHERE s.title = 'Dexter' ORDER BY tg.name ASC;
+ing 'hbtn_0d_tvshows' for listing...
+SELECT name FROM tv_genres
+JOIN tv_show_genres ON id=tv_show_genres.genre_id
+JOIN tv_shows ON tv_shows.id=tv_show_genres.show_id
+WHERE tv_shows.title = 'Dexter' ORDER BY name;
