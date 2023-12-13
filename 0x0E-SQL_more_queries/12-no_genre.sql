4c336b90b05a5c528f123ae06e804c195087d4d8
Ijbar Salah Eddine <ijbarbusiness@gmail.com>
2023-12-13 17:55:33 +0000

    Task_12

--- 0x0E-SQL_more_queries/12-no_genre.sql
+++ 0x0E-SQL_more_queries/12-no_genre.sql
@@ -1,6 +1,6 @@
 -- Listing all shows in Database 'hbtn_0d_tvshows'.
-SELECT t.title, tv_show_genres.genre_id
+SELECT t.title, tg.genre_id
 FROM tv_shows AS t
-LEFT JOIN tv_show_genres ON tv_show_genres.show_id = t.id
-WHERE tv_show_genres.show_id IS NULL
-ORDER BY t.title ASC, tv_show_genres.genre_id ASC;
+LEFT JOIN tv_show_genres AS tg ON tg.show_id = t.id
+WHERE tg.show_id IS NULL
+ORDER BY t.title ASC, tg.genre_id ASC;
