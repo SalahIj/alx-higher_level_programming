5ce7cacd80a92d253b9a71ecc97f1016e3510a5b
Ijbar Salah Eddine <ijbarbusiness@gmail.com>
2023-12-13 16:00:52 +0000

    Task_7

--- 0x0E-SQL_more_queries/7-cities.sql
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -1,5 +1,5 @@
 -- Creating database and table.
-CREATE DATABASE IF NOT EXIST hbtn_0d_usa;
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
 CREATE TABLE IF NOT EXISTS cities(
 	    id INT UNIQUE AUTO_INCREMENT NOT NULL PRIMARY KEY,
 	    state_id INT NOT NULL,
