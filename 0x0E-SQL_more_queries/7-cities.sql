32a7b2f9d0a801ea4f04e383b4a98cc99dbcd41a
Ijbar Salah Eddine <ijbarbusiness@gmail.com>
2023-12-13 16:06:10 +0000

    Task_7

--- 0x0E-SQL_more_queries/7-cities.sql
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -1,8 +1,7 @@
 -- Creating database and table.
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
-CREATE TABLE IF NOT EXISTS cities(
-	    id INT UNIQUE AUTO_INCREMENT NOT NULL PRIMARY KEY,
-	    state_id INT NOT NULL,
-	    name VARCHAR(256) NOT NULL,
-	    FOREIGN KEY(state_id) REFERENCES hbtn_0d_usa.states(id)
-	    );
+CREATE TABLE IF NOT EXISTS hbtn_0d_usa.cities (
+	       id INT UNIQUE NOT NULL AUTO_INCREMENT PRIMARY KEY,
+	       state_id INT NOT NULL,
+	       FOREIGN KEY(state_id) REFERENCES hbtn_0d_usa.states(id),
+	       name VARCHAR(256) NOT NULL);
