7e5aa95ad4c463b1fbbbdfad8aa1adb55ecbadcb
Ijbar Salah Eddine <ijbarbusiness@gmail.com>
2024-01-08 22:49:02 +0000

    Task_11

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -3,8 +3,8 @@ const len = process.argv.length;
 if (len <= 3) {
   console.log(0);
 } else {
-  const arr = process.argv.slice(2).map(Number);
-  const size = arr.length;
-  const list = arr.sort();
-  console.log(list[size - 2]);
+  const arr = process.argv.map(Number)
+    .slice(2, len)
+    .sort((a, b) => a - b);
+  console.log(arr[arr.length - 2]);
 }
