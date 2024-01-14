e153c19420657d7c6940f30a8a51f5630493db86
Ijbar Salah Eddine <ijbarsalaheddine@gmail.com>
2024-01-14 12:12:10 +0000

    tasks

--- 0x0F-python-object_relational_mapping/100-relationship_states_cities.py
+++ 0x0F-python-object_relational_mapping/100-relationship_states_cities.py
@@ -1,22 +1,24 @@
-#!/usr/bin/python3
-""" The necessery imported modules """
-import sys
+#!/usr/bin/env python3
+""" the necessery imported modules """
+from sys import argv
 from relationship_state import Base, State
 from relationship_city import City
-from sqlalchemy import create_engine
-from sqlalchemy.orm import Session
-from sqlalchemy.schema import Table
+from sqlalchemy import (create_engine)
+from sqlalchemy.orm import sessionmaker
 
 if __name__ == "__main__":
-    eng = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
-                        .format(sys.argv[1], sys.argv[2],
-                                sys.argv[3]), pool_pre_ping=True)
-    Base.metadata.create_all(eng)
+    eng = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'
+                        .format(argv[1], argv[2],
+                                argv[3]), pool_pre_ping=True)
 
-    sess = Session(eng)
-    new_city = City(name='San Francisco')
-    new = State(name='California')
-    new.cities.append(new_city)
-    sess.add_all([new, new_city])
+    Base.metadata.create_all(eng)
+    Sess = sessionmaker(bind=eng)
+    sess = Sess()
+
+    new_state = State(name="California")
+    sess.add(new_state)
+    sess.commit()
+    new_city = City(name="San Francisco", state_id=new_state.id)
+    sess.add(new_city)
     sess.commit()
     sess.close()
