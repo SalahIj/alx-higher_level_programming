d1ab91b62dacc308c27d9c8c95ea8ca9e5fa5a07
Ijbar Salah Eddine <ijbarsalaheddine@gmail.com>
2024-01-14 12:02:26 +0000

    tasks

--- 0x0F-python-object_relational_mapping/101-relationship_states_cities_list.py
+++ 0x0F-python-object_relational_mapping/101-relationship_states_cities_list.py
@@ -1,13 +1,15 @@
 #!/usr/bin/env python3
 """ The imported modules """
-import sys
-from relationship_state import Base, State
-from relationship_city import City
-from sqlalchemy import create_engine
-from sqlalchemy.orm import Session
-from sqlalchemy.schema import Table
+
 
 if __name__ == "__main__":
+    import sys
+    from relationship_state import Base, State
+    from relationship_city import City
+    from sqlalchemy import create_engine
+    from sqlalchemy.orm import Session
+    from sqlalchemy.schema import Table
+
     eng = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
                         .format(sys.argv[1], sys.argv[2],
                                 sys.argv[3]), pool_pre_ping=True)
