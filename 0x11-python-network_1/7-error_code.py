862a8a3d2ecaa44b7c01d88661d1ef674282ee6f
Ijbar Salah Eddine <ijbarbusiness@gmail.com>
2024-01-26 22:23:21 +0100

    Task_7

---
+++ 0x11-python-network_1/7-error_code.py
@@ -0,0 +1,13 @@
+#!/usr/bin/pyhon3
+""" The necessery imported modules """
+import sys
+from requests import get
+
+
+if __name__ == "__main":
+    url = sys.argv[1]
+    r_data = get(url)
+    if r_data.status_code >= 400:
+        print(r_data.status_code)
+    else:
+        print(r_data.text)
