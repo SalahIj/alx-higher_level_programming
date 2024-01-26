5cdf9b358f957140bfffe4c18644138d11109ff2
Ijbar Salah Eddine <ijbarbusiness@gmail.com>
2024-01-26 21:57:22 +0100

    Task_5

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -7,5 +7,5 @@ if __name__ == "__main__":
     url = sys.argv[1]
     email = sys.argv[2]
     payload = {'email': email}
-    r = post(url, params=payload)
+    r = post(url, data=payload)
     print(r.text)
