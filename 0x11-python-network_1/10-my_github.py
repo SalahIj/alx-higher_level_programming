2e2a8c80a157cb2b2a51deef2c9c3721d63d81cf
Ijbar Salah Eddine <ijbarbusiness@gmail.com>
2024-01-26 23:46:06 +0100

    Task_9

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -1,15 +1,11 @@
 #!/usr/bin/python3
 """ The necessery imported modules """
 import sys
-from requests import get
+import requests
 from requests.auth import HTTPBasicAuth
 
 
 if __name__ == "__main__":
-    username = sys.argv[1]
-    password = sys.argv[2]
-    url = "https://api.github.com/user"
-    authent = HTTPBasicAuth(username, password)
-    r_data = get(url, auth=authent)
-    r_js = r_data.json()
-    print(r_js['id'])
+    auth = HTTPBasicAuth(sys.argv[1], sys.argv[2])
+    r = requests.get("https://api.github.com/user", auth=auth)
+    print(r.json().get("id"))
