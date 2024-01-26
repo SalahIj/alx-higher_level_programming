bf147536f3e6d61df23e35bad818ecb2542a2aef
Ijbar Salah Eddine <ijbarbusiness@gmail.com>
2024-01-27 00:20:43 +0100

    Task_10

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -7,12 +7,13 @@ from requests import get
 if __name__ == "__main__":
     url = f"https://api.github.com/repos/{sys.argv[2]}/{sys.argv[1]}/commits"
 
-    request = get(url)
-    commits = request.json()
+    r_data = get(url)
+    com = r_data.json()
     try:
-        for i in range(10):
-            print('{}: {}'.format(
-                commits[i].get('sha'),
-                commits[i].get('commit').get('author').get('name')))
+        i = 0
+        while i < 10:
+            print(f"{com[i].get('sha')}:
+                  {com[i].get('commit').get('author').get('name')}")
+            i++
     except IndexError:
         pass
