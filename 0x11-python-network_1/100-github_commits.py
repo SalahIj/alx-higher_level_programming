6448da9d082d45a34ccbc267b4028e47dddbf697
Ijbar Salah Eddine <ijbarbusiness@gmail.com>
2024-01-27 01:24:40 +0100

    Task_10

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -11,8 +11,8 @@ if __name__ == "__main__":
     try:
         j = 0
         while j < 10:
-            print('{}: {}'.format(com[j]['sha'],
-                  com[j].get('commit').get('author').get('name')))
+            print('{}: {}'.format(c[j]['sha'],
+                  c[j].get('commit').get('author').get('name')))
             j = j + 1
     except IndexError:
         pass
