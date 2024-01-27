e3a2c07ac409eda734a6c2cefeadf76d15cc9af9
Ijbar Salah Eddine <ijbarbusiness@gmail.com>
2024-01-27 01:23:51 +0100

    Task_10

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -7,7 +7,7 @@ from requests import get
 if __name__ == "__main__":
     url = f"https://api.github.com/repos/{sys.argv[2]}/{sys.argv[1]}/commits"
     r_data = get(url)
-    com = r_data.json()
+    c = r_data.json()
     try:
         j = 0
         while j < 10:
