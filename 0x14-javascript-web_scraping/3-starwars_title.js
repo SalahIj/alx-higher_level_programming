dad45b013817c8796dcb36f91c529c872beb9260
Ijbar Salah Eddine <ijbarsalaheddine@gmail.com>
2024-02-20 19:42:30 +0000

    Task_3

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -6,17 +6,13 @@ if (process.argv.length !== 3) {
   process.exit(1);
 }
 
-const Idofmovie = process.argv[2];
+const url = `https://swapi-api.alx-tools.com/api/films/${process.argv[2]}`;
 
-const url = `https://swapi-api.alx-tools.com/api/films/${Idofmovie}`;
-
-request.get(url, (error, response, body) => {
+request.get(url, function (error, response, body) {
   if (error) {
     console.error('Error:', error);
-  } else if (response.statusCode !== 200) {
-    console.error(response.statusCode);
   } else {
-    const movie = JSON.parse(body);
-    console.log(movie.title);
+    const film = JSON.parse(body);
+    console.log(film.title);
   }
 });
