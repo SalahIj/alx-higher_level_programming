787cad9b6de27afa35af8b69739094184a7d1641
Ijbar Salah Eddine <ijbarsalaheddine@gmail.com>
2024-02-20 18:37:53 +0000

    Task_4

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -1,7 +1,19 @@
 #!/usr/bin/node
+
 const request = require('request');
-request(process.argv[2], function (error, response, body) {
-  if (!error) {
+
+if (process.argv.length !== 3) {
+  process.exit(1);
+}
+
+const Urlapi = process.argv[2];
+
+request.get(Urlapi, (error, response, body) => {
+  if (error) {
+    console.error('Error:', error);
+  } else if (response.statusCode !== 200) {
+    console.error(response.statusCode);
+  } else {
     const results = JSON.parse(body).results;
     console.log(results.reduce((count, movie) => {
       return movie.characters.find((character) => character.endsWith('/18/'))
