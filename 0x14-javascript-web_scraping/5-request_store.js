a536d656ac233684cd6bf9d16d7ba7bbec6d3519
Ijbar Salah Eddine <ijbarsalaheddine@gmail.com>
2024-02-20 19:47:00 +0000

    Task_5

--- 0x14-javascript-web_scraping/5-request_store.js
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -6,8 +6,6 @@ const request = require('request');
 request.get(process.argv[2], function (error, response, body) {
   if (error) {
     console.error('Error:', error);
-  } else if (response.statusCode !== 200) {
-    console.error(response.statusCode);
   } else {
     fs.writeFile(process.argv[3], body, 'utf-8', (err) => {
       if (err) {
