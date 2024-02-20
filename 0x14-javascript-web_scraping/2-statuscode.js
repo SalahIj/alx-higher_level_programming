68ce190433302daf22a421a9da252efacdb4825f
Ijbar Salah Eddine <ijbarsalaheddine@gmail.com>
2024-02-20 17:54:52 +0000

    Task_2

---
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -0,0 +1,17 @@
+#!/usr/bin/node
+
+const request = require('request');
+
+if (process.argv.length !== 3) {
+  process.exit(1);
+}
+
+const url = process.argv[2];
+
+request.get(url, (error, response) => {
+  if (error) {
+    console.error('Error:', error);
+  } else {
+    console.log(`code: ${response.statusCode}`);
+  }
+});
